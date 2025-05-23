4407385813c275cbf296b3c21a9dfb8fd7a8abdf
Simone Taeggi <startae14@gmail.com>
2025-05-23 11:50:39 +0200

    04: Creating the ProgressBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,43 @@
 'use client';
 
-import EducationBackground from "@/components/EducationBackground";
-import ExperienceInfo from "@/components/ExperienceInfo";
-import PersonalInfo from "@/components/PersonalInfo";
-import ReviewSubmit from "@/components/ReviewSubmit";
-import useJobAppStore from "@/store";
-
+import EducationBackground from '@/components/EducationBackground';
+import ExperienceInfo from '@/components/ExperienceInfo';
+import PersonalInfo from '@/components/PersonalInfo';
+import ProgressBar from '@/components/ProgressBar';
+import ReviewSubmit from '@/components/ReviewSubmit';
+import useJobAppStore from '@/store';
 
 export default function Home() {
-  const { step } = useJobAppStore();
+	const { step } = useJobAppStore();
 
-  const renderStep = () => {
-    switch (step) {
-      case 1:
-        return <PersonalInfo />;
-      case 2:
-        return <ExperienceInfo />;
-      case 3:
-        return <EducationBackground />;
-      case 4:
-        return <ReviewSubmit />;
-      default:
-        return null;
-    }
-  };
+	const renderStep = () => {
+		switch (step) {
+			case 1:
+				return <PersonalInfo />;
+			case 2:
+				return <ExperienceInfo />;
+			case 3:
+				return <EducationBackground />;
+			case 4:
+				return <ReviewSubmit />;
+			default:
+				return null;
+		}
+	};
 
-  return (
-    <div className="flex justify-center py-10">
-      <div className="bg-gray-100 rounded-lg w-full p-10 mx-5">
-        <div className="pt-2 mb-5 text-center text-2xl sm:text-3xl font-semibold">
-          Job Application
-        </div>
-      
-        {/* Progress Bar */}
-      
-        {/* Steps */}
-        <div>{renderStep()}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex justify-center py-10">
+			<div className="bg-gray-100 rounded-lg w-full p-10 mx-5">
+				<div className="pt-2 mb-5 text-center text-2xl sm:text-3xl font-semibold">
+					Job Application
+				</div>
+
+				{/* Progress Bar */}
+				<ProgressBar />
+
+				{/* Steps */}
+				<div>{renderStep()}</div>
+			</div>
+		</div>
+	);
 }
--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -1,7 +1,49 @@
+import useJobAppStore from '@/store';
+
+const Circle = ({ step, currentIndex }: { step: number; currentIndex: number }) => {
+	return (
+		<div
+			className={`w-7 h-7 sm:w-10 sm:h-10 flex items-center justify-center rounded-full border-2 sm:border-4 select-none transition-colors duration-300 ease-in-out delay-300 ${
+				step === currentIndex
+					? 'text-blue-500 border-blue-500'
+					: `${
+							step < currentIndex
+								? 'bg-white border-gray-300 text-gray-500'
+								: 'bg-blue-500 border-blue-500 text-white'
+					  }`
+			}`}
+		>
+			{currentIndex}
+		</div>
+	);
+};
+
 function ProgressBar() {
-  return (
-    <div>ProgressBar</div>
-  )
+	const { step, getTotalSteps } = useJobAppStore();
+	const totalSteps = getTotalSteps();
+	return (
+		<div className="flex mx-auto justify-between mb-6 w-3/4 max-w-2xl">
+			{[...Array(totalSteps - 1)].map((_, idx) => (
+				<div key={idx} className="flex items-center w-full">
+					<Circle step={step} currentIndex={idx + 1} />
+					{/* connecting lines */}
+					<div className="flex-grow h-[2px] sm:h-1 relative">
+						<div className="absolute top-0 left-0 h-full w-full bg-gray-300" />
+						{/* blue line */}
+						<div
+							className={`absolute top-0 left-0 h-full w-full bg-blue-500 transition-transform duration-300 ease-in-out origin-left transform ${
+								step > idx + 1 ? 'scale-x-100' : 'scale-x-0'
+							}`}
+						/>
+					</div>
+				</div>
+			))}
+			{/* we add the last circle because we don't need to show a line starting from it */}
+			<div className="flex items-center w-fit">
+				<Circle step={step} currentIndex={totalSteps} />
+			</div>
+		</div>
+	);
 }
 
-export default ProgressBar
+export default ProgressBar;
